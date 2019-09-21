fecf573dcc0e032d828b4148460e5ac6d518e5be
huangcheng <huangcheng.job@foxmail.com>
2019-09-21 17:09:18 +0800

    Update 100ask_rc-nec.c

--- drivers/media/rc/100ask_rc-nec.c
+++ drivers/media/rc/100ask_rc-nec.c
@@ -1,36 +1,33 @@
 #include <media/rc-map.h>
 #include <linux/module.h>
 
-static struct rc_map_table hceng_nec[] = {
-    { 0x45, KEY_CHANNELDOWN},
-    { 0x46, KEY_CHANNEL},
-    { 0x47, KEY_CHANNELUP},
-    { 0x44, KEY_PREVIOUS},
-    { 0x40, KEY_NEXT},
-    { 0x43, KEY_PLAYPAUSE}, //
-    { 0x07, KEY_VOLUMEDOWN},
-    { 0x15, KEY_VOLUMEUP},
-    { 0x09, KEY_EQUAL},
-    
+static struct rc_map_table rc_100ask_nec[] = {
+    { 0x45, KEY_POWER},
+    { 0x47, KEY_MENU},
+    { 0x44, KEY_UNKNOWN},
+    { 0x40, KEY_VOLUMEUP},
+    { 0x43, BTN_BACK},
+    { 0x07, KEY_PREVIOUS}, 
+    { 0x15, KEY_PLAYPAUSE},
+    { 0x09, KEY_NEXT},
     { 0x16, KEY_0},
-    { 0x19, KEY_F1},
-    { 0x0d, KEY_F2},
-    
-    { 0x0c, KEY_1},
+    { 0x19, KEY_VOLUMEDOWN},
+    { 0x0D, KEY_CLEAR},
+    { 0x0C, KEY_1},
     { 0x18, KEY_2},
-    { 0x5e, KEY_3},
+    { 0x5E, KEY_3},
     { 0x08, KEY_4},
-    { 0x1c, KEY_5},
-    { 0x5a, KEY_6},
+    { 0x1C, KEY_5},
+    { 0x5A, KEY_6},
     { 0x42, KEY_7},
-    { 0x52, KEY_8}, //
-    { 0x4a, KEY_9},
+    { 0x52, KEY_8}, 
+    { 0x4A, KEY_9}, 
 };
 
 static struct rc_map_list nec_100ask_map = {
     .map = {
-        .scan    = hceng_nec,
-        .size    = ARRAY_SIZE(hceng_nec),
+        .scan    = rc_100ask_nec,
+        .size    = ARRAY_SIZE(rc_100ask_nec),
         .rc_type = RC_TYPE_NEC, //RC_TYPE_UNKNOWN //echo nec > /sys/class/rc/rc0/protocols
         .name    = "rc-100ask-nec",
     }
